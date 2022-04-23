c655bdfdadda4c5859e76eeaa9c6fa56102cb02d
Saman <samanpreet.pannu1126@gmail.com>
2022-04-22 19:49:53 -0700

    Updates

---
+++ precache-manifest.fc60d7cb460614bc7d53050c8312356b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9331d4f342c34300227c2e2df1473f78",
+    "url": "/aromaa/index.html"
+  },
+  {
+    "revision": "07ba68bf00795fb4adee",
+    "url": "/aromaa/static/css/main.ff3a3a3d.chunk.css"
+  },
+  {
+    "revision": "9a32938beeea90f3be11",
+    "url": "/aromaa/static/js/2.7aefca5b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/aromaa/static/js/2.7aefca5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ba68bf00795fb4adee",
+    "url": "/aromaa/static/js/main.832da6c7.chunk.js"
+  },
+  {
+    "revision": "fb28815dc500965cd20c",
+    "url": "/aromaa/static/js/runtime-main.3e3cf6fe.js"
+  }
+]);
